7ea8e2986c2a6addd5311fb8bce5babe98c34724
Ashish1 <Ashish1@Ashish1-PC>
2020-03-27 14:20:39 +0530

    First Commit

---
+++ LearnSpringBoot/src/test/java/learn/springboot/LearnSpringBoot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.springboot.LearnSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
